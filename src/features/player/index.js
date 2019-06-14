0c08ccc1e94c6d581a50f98163eadfedbeed7d19
Smith, Tom (UK London) <tom.smith5@baesystems.com>
2019-06-14 13:34:09 +0100

    Added Player

--- src/features/player/index.js
+++ src/features/player/index.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import walkSprite from "../../img/walkSprite.png";
 
 function player(props) {
-  return null;
+  return (
+    <div
+      style={{
+        position: "relative",
+        top: props.position[1],
+        left: props.position[0],
+        backgroundImage: `url('${walkSprite}')`,
+        backgroundPosition: "0 0",
+        width: "40px",
+        height: "60px"
+      }}
+    />
+  );
 }
 
-export default player;
+function mapStateToProps(state) {
+  return {
+    ...state.player
+  };
+}
+
+export default connect(mapStateToProps)(player);
